8d0c9fcce066efce258e38ad305f7c185d1babf5
Allana Anderson <allana.anderson17@gmail.com>
2020-08-21 16:27:56 -0600

    added api file, created search bar component, formatted index.js.

---
+++ src/Components/Search.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Search(props) {
+  return (
+    <>
+      <div class="input-group input-group-sm mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="inputGroup-sizing-sm">
+            Search
+          </span>
+        </div>
+        <input
+          type="text"
+          class="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-sm"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Search;

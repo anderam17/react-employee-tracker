8ba759727ecae2112f6e478d001aa5bb570a84f5
Allana Anderson <allana.anderson17@gmail.com>
2020-08-23 20:14:41 -0600

    added minor formatting changes.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,7 +8,7 @@ function Header(props) {
     <div className="jumbotron jumbotron-fluid">
     <div className="container">
       <h1 className="display-4" id="big-title">Employee Search</h1>
-      <p className="lead" id="big-subtitle"> Search Employees by entering their first or full name in the search box. 
+      <p className="lead" id="big-subtitle"> Search Employees by entering their first or last name in the search box. 
       <br/> 
       Filter by name by clicking the arrow in "name" column.
       <br/> 

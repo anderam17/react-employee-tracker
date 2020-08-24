3a2b42cc2235956a9139cfd484f1fd862122b579
Allana Anderson <allana.anderson17@gmail.com>
2020-08-23 20:56:00 -0600

    testing bug fix

---
+++ build/precache-manifest.6d246b4f131c56b45a4c08542f0ed2ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "775f70d6e5cf07d40399d388e17f2d9d",
+    "url": "/react-employee-tracker/index.html"
+  },
+  {
+    "revision": "3c14ddd2f05a6d63a4e0",
+    "url": "/react-employee-tracker/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e895fcb54d25625f74f0",
+    "url": "/react-employee-tracker/static/css/main.a36f9167.chunk.css"
+  },
+  {
+    "revision": "3c14ddd2f05a6d63a4e0",
+    "url": "/react-employee-tracker/static/js/2.67e61d1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-tracker/static/js/2.67e61d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e895fcb54d25625f74f0",
+    "url": "/react-employee-tracker/static/js/main.98b006c5.chunk.js"
+  },
+  {
+    "revision": "7602196a44d02f3705a7",
+    "url": "/react-employee-tracker/static/js/runtime-main.e63658e6.js"
+  }
+]);
